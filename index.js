0d9e159c2697ba85b806b6f7f31706b240d47f87
Jaykita Bunareerojana <vandyke.rijn@gmail.com>
2020-05-31 16:29:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+var tweet = prompt("Compose your tweet here!");
+var tweetCount = tweet.length;
+alert("You have composed " + tweetCount + " characters, now you have " + (140-tweetCount) + " characters left.")
